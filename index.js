20b0373f2cd50d51cc13cdcc796da7fff3c88dc2
jackarms <90922918+jackarms@users.noreply.github.com>
2022-01-23 18:18:44 -0600

    creation of inquirer prompt flow

--- index.js
+++ index.js
@@ -4,58 +4,85 @@ const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
 const managerQuestions = function () {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "manager_name",
-      message: "What is your team manager's name?",
-    },
-    {
-      type: "number",
-      name: "manager_id",
-      message: "What is your team manager's ID number?",
-    },
-    {
-      type: "input",
-      name: "manager_email",
-      message: "What is your team manager's email address?",
-    },
-    {
-      type: "number",
-      name: "office_number",
-      message: "What is your team manager's office number?",
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "manager_name",
+        message: "What is your team manager's name?",
+      },
+      {
+        type: "number",
+        name: "manager_id",
+        message: "What is your team manager's ID number?",
+      },
+      {
+        type: "input",
+        name: "manager_email",
+        message: "What is your team manager's email address?",
+      },
+      {
+        type: "number",
+        name: "office_number",
+        message: "What is your team manager's office number?",
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      employeeQuestions();
+    });
 };
+managerQuestions();
 
 const employeeQuestions = function () {
-  console.log(`New Employee Questions!`);
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "pick_employee",
-      message: "Would you like to add another employee to the team?",
-      choices: [
-        "engineer",
-        "intern",
-        "no, I would not like to add another employee.",
-      ],
-    },
-  ]);
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "pick_employee",
+        message: "Would you like to add another employee to the team?",
+        choices: [
+          "engineer",
+          "intern",
+          "no, I would not like to add another employee.",
+        ],
+      },
+    ])
+    .then((answers) => {
+      if (answers.pick_employee == "intern") {
+        internQuestions();
+      } else if (answers.pick_employee == "engineer") {
+        engineerQuestions();
+      } else {
+        console.log(`Your team has been created!`);
+      }
+    });
 };
-
-managerQuestions()
-  .then((answers) => {
-    let manager = new Manager(
-      answers.manager_name,
-      answers.manager_id,
-      answers.manager_email,
-      answers.office_number
-    );
-    return manager;
-    console.log(manager);
-  })
-  .then(employeeQuestions)
-  .then((projectAnswers) => console.log(projectAnswers.pick_employee));
-
-console.log(answers);
+const internQuestions = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "school",
+        message: "Which school did you attend?",
+      },
+    ])
+    .then((schoolAnswers) => {
+      console.log(schoolAnswers);
+      employeeQuestions();
+    });
+};
+const engineerQuestions = function () {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+      },
+    ])
+    .then((githubAnswers) => {
+      console.log(githubAnswers);
+      employeeQuestions();
+    });
+};
